700ae004aaadfa7fd207259334325169b3c520f3
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2018-03-23 21:59:16 -0700

    Update rasppi_subscriber_node.py

--- scripts/rasppi_subscriber_node.py
+++ scripts/rasppi_subscriber_node.py
@@ -4,34 +4,20 @@ import google.cloud
 import os
 from std_msgs.msg import String
 
-def publishFileName()
-	pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('listener', anonymous=True)
-	rate = rospy.rate=10
-	
-	while not rospy.is_shutdown()
-		hello_str = "hello world %s" % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
-		rate.sleep()
-
-
-pub = rospy.Publisher('student2', String, queue_size=10)
-
 
 def callback(data):
 	my_str = data.data
-	pub.publish(my_str)
+	engine = pttsx3.init()
+	engine.say(my_str)
+	engine.runAndWait()
         
 		
 def listener():
-    rospy.init_node('telephone1', anonymous=True)
-    rospy.Subscriber("student1", String, callback)
+    rospy.init_node('speaker', anonymous=True)
+    rospy.Subscriber("response_text", String, callback)
     rospy.spin()
+
 
 if __name__ == 
 '__main__':
-	try:
-    	talker()
-	except rospy.ROSInterruptException:
-		pass
+	listener()
