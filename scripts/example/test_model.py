74d2d0077f85c34a4e06f58c084a5b45790147c9
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2018-03-25 21:32:05 -0700

    Update test_model.py

--- scripts/example/test_model.py
+++ scripts/example/test_model.py
@@ -1,3 +1,50 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import re
+import time
+tf.__version__
+
+def clean_text(text):
+    '''Clean text by removing unnecessary characters and altering the format of words.'''
+
+    text = text.lower()
+    
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "that is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"n'", "ng", text)
+    text = re.sub(r"'bout", "about", text)
+    text = re.sub(r"'til", "until", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    
+    return text
+
+ 
+def question_to_seq(question, vocab_to_int):
+    '''Prepare the question for the model'''
+    question = clean_text(question)
+    return [vocab_to_int.get(word, vocab_to_int['<UNK>']) for word in question.split()]
+
+
+sess = tf.Session()
+new_saver = tf.train.import_meta_graph('my-model.meta')
+new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+all_vars = tf.get_collection('vars')
+
 # Prepare the question
 input_question = question_to_seq(input_question, questions_vocab_to_int)
 
